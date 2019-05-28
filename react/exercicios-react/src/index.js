fee024db011fdfaed3047fe8811fa3983f22f464
Leonardo Costa <leocosta@edu.unifor.br>
2019-05-28 11:10:42 -0300

    Código do relogio

--- react/exercicios-react/src/index.js
+++ react/exercicios-react/src/index.js
@@ -14,3 +14,45 @@ ReactDOM.render(
     <FamiliaFuncional membros={['Claudio', 'Marcela', 'Thomaz']} sobrenome='Funcional'/>
     </>
 , document.getElementById('root'))
+
+
+
+// import React, { Component } from 'react'
+// import { render, unmountComponentAtNode } from 'react-dom'
+// import moment from 'moment'
+
+// class Clock extends Component {
+//     constructor() {
+//         super()
+//         this.state = { time : moment().format('D. MMMM YYYY H:mm:ss') }
+//     }
+
+//     componentDidMount() {
+//         console.log('Starting Clock');
+//         this.ticking = setInterval( _ => 
+//             this.setState({ time : moment().format('DD MMMM YYYY H:mm:ss') })
+//             , 1000
+//         )
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.ticking)
+//         console.log('Stopping Clock');
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.props.onClose}>X</button>
+//                 <h1>{this.state.time}</h1>
+//             </div>
+            
+//         )
+//     }
+// }
+
+// const target = document.getElementById('root')
+
+// render(
+//    <Clock onClose={ _ => unmountComponentAtNode(target)} /> 
+// , target)
