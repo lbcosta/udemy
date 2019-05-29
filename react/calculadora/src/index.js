4a7c7c53d54011058836a07d0a5fea9c7f3e5faa
Leonardo Costa <leocosta@edu.unifor.br>
2019-05-29 15:27:46 -0300

    Inicio do projeto Calculadora

---
+++ react/calculadora/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Calculator from './components/Calculator/Calculator'
+
+ReactDOM.render(
+    <>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </>
+, document.getElementById('root'));
